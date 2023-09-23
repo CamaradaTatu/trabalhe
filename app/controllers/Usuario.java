51b5266a67119cf2ffde32cf68b718da7bdf6552
Ruanderson Carlos Menezes de Melo <ruanderson123@gmail.com>
2023-09-23 11:28:17 -0300

    Initial commit

---
+++ app/controllers/Usuario.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Usuario extends Controller {
+	
+	public void login() {
+		render();
+	}
+	public void cadastrar() {
+		render();
+	}
+	
+	public void telaInicial() {
+		render();
+	}
+	public void listaJogos() {
+		render();
+		
+	}
+	public void listaComentario() {
+		render();
+	}
+
+}
